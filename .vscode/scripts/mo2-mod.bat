da0075963b9331894c3903d434a6087319be784c
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-03-04 23:13:49 -0700

    tweak build script

--- .vscode/scripts/mo2-mod.bat
+++ .vscode/scripts/mo2-mod.bat
@@ -1,1 +1,1 @@
-robocopy "dist\Object Categorization Framework\Base" "C:\Games\Skyrim\MO2\mods\Object Categorization Framework (pre-release) []" /e
+robocopy "dist\Object Categorization Framework\Base" "C:\Games\Skyrim\MO2\mods\Object Categorization Framework (pre-release) []" /e /xf meta.ini
