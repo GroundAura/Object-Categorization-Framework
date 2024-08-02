1f9ae75c1d53ca2442b1aaef6a77f6d20c2a884e
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-08-02 05:51:34 -0600

    build path fixes

--- .vscode/scripts/mo2-mod.bat
+++ .vscode/scripts/mo2-mod.bat
@@ -1,1 +1,1 @@
-robocopy "dist\Object Categorization Framework\Base" "C:\Games\Skyrim\MO2\mods\Object Categorization Framework (pre-release) []" /e /xf meta.ini
+robocopy "dist\Object Categorization Framework\Base" "E:\Skyrim\MO2\mods\Object Categorization Framework (pre-release) []" /e /xf meta.ini
